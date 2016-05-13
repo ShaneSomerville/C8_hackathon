f4032d01965087c565237910b62297977862b68a
Andrew Kim <jeesoo2002@gmail.com>
2016-05-12 18:01:59 -0700

    house img append added

--- div6_andrew.js
+++ div6_andrew.js
@@ -30,13 +30,15 @@ function getCarPhoto() {
         },
         success: function (result) {
             console.log(result);
-            console.log('request has been made for ',  result.photos.photo[0].id);
+            console.log('car request has been made for ',  result.photos.photo[0].id);
+            var index = Math.floor((Math.random() * 25));
+            console.log("index " + index);
 
             var all_photo = result.photos.photo;
-            var photo_id = all_photo[0].id;
-            var farm_id = all_photo[0].farm;
-            var secret_id = all_photo[0].secret;
-            var server_id = all_photo[0].server;
+            var photo_id = all_photo[index].id;
+            var farm_id = all_photo[index].farm;
+            var secret_id = all_photo[index].secret;
+            var server_id = all_photo[index].server;
 
             console.log(photo_id, farm_id, secret_id);
             var image_src = 'https://farm' + farm_id +'.staticflickr.com/'
@@ -58,27 +60,29 @@ function getHomePhoto() {
             method: 'flickr.photos.search',
             api_key: '3c46705eb50f63815c46f96aa8ce171d',
             nojsoncallback: '1',
-            text: 'car',
+            text: 'house -bird',
             format: 'json'
         },
         success: function (result) {
             console.log(result);
-            console.log('request has been made for ',  result.photos.photo[0].id);
+            console.log('home request has been made for ',  result.photos.photo[0].id);
+            var index = Math.floor((Math.random() * 25));
+            console.log("index " + index);
 
             var all_photo = result.photos.photo;
-            var photo_id = all_photo[0].id;
-            var farm_id = all_photo[0].farm;
-            var secret_id = all_photo[0].secret;
-            var server_id = all_photo[0].server;
+            var photo_id = all_photo[index].id;
+            var farm_id = all_photo[index].farm;
+            var secret_id = all_photo[index].secret;
+            var server_id = all_photo[index].server;
 
             console.log(photo_id, farm_id, secret_id);
             var image_src = 'https://farm' + farm_id +'.staticflickr.com/'
                 + server_id +'/'+ photo_id +'_'+ secret_id +'.jpg';
             console.log(image_src);
 
-            var car_image = $('<img>').attr('src', image_src).addClass('car_img');
+            var house_image = $('<img>').attr('src', image_src).addClass('house_img');
 
-            $('.div6').append(car_image);
+            $('.div6').append(house_image);
         }
     })
 }
